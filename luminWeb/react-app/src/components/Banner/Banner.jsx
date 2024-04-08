3e52e0609f1574c3e9f2630aafcd143463b74049
Shovan Mazumder <shovan2192@gmail.com>
2024-04-09 00:16:50 +0600

    Update Banner

--- luminWeb/react-app/src/components/Banner/Banner.jsx
+++ luminWeb/react-app/src/components/Banner/Banner.jsx
@@ -7,7 +7,7 @@ const Banner = () => {
       <div className='banner-text'>
         <h1 className='mb-4'>Lumen Corporation</h1>
         <p className='mb-4'>We ensure better services for better world.</p>
-        <button className='btn-primary'>Explore more </button>
+        <button className='btn-primary'>About Us</button>
       </div>
     </div>
   )
